fd7ea72274a0e18d052fe8eb7403cd9bd05ac59f
kylezs <zsembery.kyle@gmail.com>
2018-12-06 19:43:39 +1100

    Styling of subscribe done, hopefully form submission will work

--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,27 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { Container } from 'semantic-ui-react'
+import { Container, Form, Input, Grid } from 'semantic-ui-react'
+import './header.css'
 
 const Header = ({ siteTitle }) => (
   <div style={{ background: '#5AB9EA' }}>
     <Container>
-      <h1 style={{ padding: '1rem 0', marginBottom: '2rem' }}>
+    <Grid style={{ paddingBottom: 0, margin: 0 }} stackable>
+    <Grid.Row columns={2} style={{ padding: 0, marginBottom: 0 }}>
+    <Grid.Column style={{ paddingBottom: 0, marginBottom: 0 }}>
+      <h1 style={{ padding: '1rem 0', marginBottom: 0 }}>
         <Link style={{ color: 'white', fontWeight: '20', fontSize: '1.4em' }} to="/">
           {siteTitle}
         </Link>
       </h1>
+    </Grid.Column>
+    <Grid.Column verticalAlign='middle'>
+      <Form name="subscribe" netlify>
+      <Input className="email-input" fluid transparent style={{ paddingLeft: '4px' }} type="email" name="email" placeholder="Enter email" action="Subscribe" />
+      </Form>
+      </Grid.Column>
+      </Grid.Row>
+    </Grid>
     </Container>
   </div>
 )
